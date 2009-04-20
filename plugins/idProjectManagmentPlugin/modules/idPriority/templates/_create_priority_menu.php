596a39a124deafe22be1fbe8be6663ac604f654f
Filippo <filippo@paperino.(none)>
2009-04-20 23:04:34 +0200

    Removing wrong folders

--- plugins/idProjectManagmentPlugin/modules/idPriority/templates/_create_priority_menu.php
+++
@@ -1,7 +0,0 @@
-<div class="secondary-navigation">
-  <ul>
-    <li class="first active"><?php echo link_to( __('Create new priority'), '@new_priority') ?></li>
-    <li><?php echo link_to( __('Priority list'), '@index_priority') ?></li>
-  </ul>
-  <div class="clear"></div>
-</div>
--- plugins/idProjectManagmentPlugin/modules/idProject/templates/_sf_guard_form_footer.php
+++
@@ -1,8 +0,0 @@
-<div class="actions-bar">
-  <div class="actions">
-    <?php echo (!$form->isNew()) ? link_to(__('Delete'), '@sf_guard_'.$module_name.'_delete?id='.$form->getObject()->getId(), array('confirm' => 'Do you really want to delete this '.$module_name.'?')).' | ' : '' ;?>
-    <?php echo link_to(__('Cancel'), 'sf_guard_'.$module_name); ?> |
-    <?php echo input_tag('Submit', ($form->isNew()) ? 'Save' : 'Save modification', array('class' => 'button', 'type' => 'submit'))?>
-  </div>
-  <div class="clear"></div>
-</div>
